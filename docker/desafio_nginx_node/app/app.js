cec9c5e2f49ee8976f43b1ab9d9a49afbdfdedac
rimagawa <rimagawa@gmail.com>
2024-10-09 12:39:54 -0300

    docker compose funcionando

--- docker/desafio_nginx_node/app/app.js
+++ docker/desafio_nginx_node/app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const con = mysql.createConnection({
 	host: 'db',
@@ -13,7 +13,7 @@ con.connect(function (err) {
 	if (err) throw err;
 	console.log('Connected!');
 	const sql =
-		'CREATE TABLE users (name VARCHAR(255), id INT NOT NULL AUTO_INCREMENT)';
+		'CREATE TABLE users (name VARCHAR(255), id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))';
 	con.query(sql, function (err, result) {
 		if (err) throw err;
 		console.log('Table users created');
@@ -25,18 +25,27 @@ con.connect(function (err) {
 		console.log('1 record inserted');
 	});
 });
-
-app.get('/', (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log('Connected!');
-		const sql = 'SELECT name FROM users';
-		con.query(sql, function (err, result) {
+function firstFunction() {
+	return new Promise((resolve, reject) => {
+		let lista = [];
+		con.connect(function (err) {
 			if (err) throw err;
-			console.log(result);
+			con.query('SELECT name FROM users', async function (err, result, fields) {
+				if (err) throw err;
+				result.forEach(element => {
+					lista.push(element['name']);
+				});
+				// get the results from post table
+				console.log('Loop completed.');
+				resolve(lista); // <---- Resolve the promise here
+			});
 		});
 	});
-	res.send('<h1>Full Cycle Rocks!</h1>');
+}
+
+app.get('/', async (req, res) => {
+	const result = await firstFunction()
+	res.send('<h1>Full Cycle Rocks!</h1><br><p>' + result + '</p>');
 });
 
 const PORT = 4400;
